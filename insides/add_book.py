51f2e22f7a524b59dfe184b5c8e436f68193ce39
ksolovejus <solovejuskarolis@gmail.com>
2024-10-10 09:24:08 +0300

    working add_book

--- insides/add_book.py
+++ insides/add_book.py
@@ -4,9 +4,30 @@ class AddBook:
     def __init__(self, book_collection):
         self.book_collection = book_collection
 
+        # Preset of books 
+        self.preset_books = [
+            ("The Great Gatsby", "F. Scott Fitzgerald", 1925, "Classic"),
+            ("1984", "George Orwell", 1949, "Dystopian"),
+            ("To Kill a Mockingbird", "Harper Lee", 1960, "Classic"),
+            ("Moby Dick", "Herman Melville", 1851, "Adventure"),
+            ("Pride and Prejudice", "Jane Austen", 1813, "Romance"),
+            ("The Catcher in the Rye", "J.D. Salinger", 1951, "Fiction"),
+            ("The Hobbit", "J.R.R. Tolkien", 1937, "Fantasy"),
+            ("War and Peace", "Leo Tolstoy", 1869, "Historical Fiction"),
+            ("The Odyssey", "Homer", -800, "Epic Poetry"),
+            ("Crime and Punishment", "Fyodor Dostoevsky", 1866, "Philosophical Fiction"),
+        ]
+
     def add_book(self):
         while True:
             pavadinimas = input("Iveskite pavadinima: ")
+            if pavadinimas == "def":
+                for title, author, year, genre in self.preset_books:
+                    knyga = Library(title, author, year, genre)
+                    self.book_collection.library.append(knyga)
+                print("Default book list added!")
+                break
+
             autorius = input("Iveskite autoriu: ")
             isleidimo_metai = float(input("Iveskite isleidimo metus: "))
             zanras = input("Iveskite zanra: ")
@@ -20,8 +41,11 @@ class AddBook:
                 break
 
     def display_books(self):
+        i = 1
         for book in self.book_collection.library:
-            print(f"{book.pavadinimas} by {book.autorius}, {book.isleidimo_metai}, {book.zanras}")
+            print("-" * 70)
+            print(f"{i}. {book.pavadinimas} by {book.autorius}, {book.isleidimo_metai}, {book.zanras}")
+            i += 1
 
 
 my_books = MyBooks()
